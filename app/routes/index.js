e00e26d7cab9023075d041f2cc59b4e87be9111c
Richard Saldivar <rsaldiva@gmail.com>
2016-03-23 17:23:06 +0000

    update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+
 //var timeHandler = require(path + '/app/controllers/timekHandler.server.js');
 
 
@@ -16,7 +16,7 @@ module.exports = function(app, passport) {
   	}*/
 
 
-  var clickHandler = new ClickHandler();
+
 
   app.route('/')
     .get(function(req, res) {
@@ -116,33 +116,5 @@ module.exports = function(app, passport) {
 
 
 
-  app.route('/logout')
-    .get(function(req, res) {
-
-      res.redirect('/login');
-    });
-
-  app.route('/profile')
-    .get(function(req, res) {
-      res.sendFile(path + '/public/profile.html');
-    });
-
-  app.route('/api/:id')
-    .get(function(req, res) {
-      res.json(req.user.github);
-    });
-
-  app.route('/auth/github')
-    .get(passport.authenticate('github'));
-
-  app.route('/auth/github/callback')
-    .get(passport.authenticate('github', {
-      successRedirect: '/',
-      failureRedirect: '/login'
-    }));
-
-  /*app.route('/api/:id/clicks')
-  	.get(isLoggedIn, clickHandler.getClicks)
-  	.post(isLoggedIn, clickHandler.addClick)
-  	.delete(isLoggedIn, clickHandler.resetClicks);*/
+  
 };
